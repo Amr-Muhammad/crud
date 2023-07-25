14e609a57f1eade2d42a45fbbb0f844d8b975074
Mik00z <amr.casepr.98@gmail.com>
2023-07-25 19:33:56 +0300

    b7awl azbt el search array, zbbt el delete mn el js msh mn el html

--- js/index.js
+++ js/index.js
@@ -4,9 +4,10 @@ var ProductCategoryInput = document.getElementById("ProductCategory")
 var ProductDescriptionInput = document.getElementById("ProductDescription")
 var searchInput = document.getElementById("searchInput")
 
+
+var b = 0
 var productsArray = []
-var temp = ""
-var amr = 0
+var currentIndex = 0
 
 document.getElementById("edit").style.display = "none"
 document.getElementById("regexName").style.display = "none"
@@ -16,6 +17,12 @@ document.getElementById("regexPrice").style.display = "none"
 if (localStorage.getItem("Products") != null) {
     productsArray = JSON.parse(localStorage.getItem("Products"));
     displayProduct(productsArray);
+    eventHandler();
+}
+
+if (localStorage.getItem("B") != null) {
+    b = JSON.parse(localStorage.getItem("B"))
+    console.log(b);
 }
 
 function addProduct() {
@@ -31,15 +38,15 @@ function addProduct() {
         productsArray.push(product);
         localStorage.setItem("Products", JSON.stringify(productsArray))
         displayProduct(productsArray);
+        localStorage.setItem("B", JSON.stringify(b))
         clearForm();
-
+        eventHandler();
     }
-
 }
 
 function displayProduct(arrayParameter) {
     var temp = ""
-    for (var i = 0; i < arrayParameter.length; i++) {
+    for (var i = 0; i < arrayParameter.length; i++, b++) {
         temp = temp + `<tr>
     <td class = "color text-center">${i}</td>
     
@@ -52,23 +59,38 @@ function displayProduct(arrayParameter) {
     <td>${(arrayParameter[i].description.charAt(0).toUpperCase() + arrayParameter[i].description.slice(1).toLowerCase()).replace((searchInput.value.charAt(0).toUpperCase() + searchInput.value.slice(1).toLowerCase()), `<span class="bg-info">${(searchInput.value.charAt(0).toUpperCase() + searchInput.value.slice(1).toLowerCase())}</span>`)
             }</td >
     <td> <a href="#" onclick="update(${i})" class="bg-color btn bg-warning"><i class="fa-solid fa-pen-to-square"></i></a></td>
-    <td> <button onclick="deleteProduct(${i})" class="btn bg-danger"><i class="fa-solid fa-trash"></i></button></td>
+    <td> <button infiniteIndex="${b}" class="btn bg-danger"><i class="fa-solid fa-trash"></i></button></td>
     </tr > `
     }
     document.getElementById("productsContent").innerHTML = temp
 }
 
-function deleteProduct(i) {
-    productsArray.splice(i, 1);
+
+function eventHandler() {
+    var deleteBtns = document.querySelectorAll("[infiniteIndex]")
+    for (var i = 0; i < productsArray.length; i++) {
+        deleteBtns[i].addEventListener("click", function (e) {
+            var infiniteIndex = Number(e.target.getAttribute("infiniteIndex"))
+            deleteProduct(infiniteIndex)
+        })
+    }
+}
+
+
+function deleteProduct(infiniteIndex) {
+    productsArray.splice(infiniteIndex, 1);
     localStorage.setItem("Products", JSON.stringify(productsArray))
     displayProduct(productsArray);
+    eventHandler();
 }
+
 
 function searchProducts() {
     var matchedProductsArray = []
 
     for (var i = 0; i < productsArray.length; i++) {
         if (productsArray[i].name.toLowerCase().includes(searchInput.value.toLowerCase()) == true) {
+
             matchedProductsArray.push(productsArray[i])
         }
     }
@@ -85,7 +107,7 @@ function update(i) {
 
     document.getElementById("addProductelement").style.display = "none";
     document.getElementById("edit").style.display = "inline-block";
-    amr = i
+    currentIndex = i
 }
 
 function clearForm() {
@@ -101,10 +123,10 @@ function editProducts() {
     document.getElementById("edit").style.display = "none";
 
 
-    productsArray[amr].name = productNameInput.value
-    productsArray[amr].price = productPriceInput.value
-    productsArray[amr].category = ProductCategoryInput.value
-    productsArray[amr].description = ProductDescriptionInput.value
+    productsArray[currentIndex].name = productNameInput.value
+    productsArray[currentIndex].price = productPriceInput.value
+    productsArray[currentIndex].category = ProductCategoryInput.value
+    productsArray[currentIndex].description = ProductDescriptionInput.value
 
     localStorage.setItem("Products", JSON.stringify(productsArray))
 
@@ -114,9 +136,16 @@ function editProducts() {
 
 
 
-//! lma ados 3la update y3ml scroll up w y focus el screen
-//? lma a3ml search 3la 7aga w agrb ams7 aw a edit el index elly shaylo el zrayr byt8yr 
-//! ezay fi goz2 el display lma a search 3la 7aga mn nos elkelma y3mlha replace brdo blon mo5tlf
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -152,4 +181,5 @@ function checkProductPrice() {
         return false;
     }
 }
+
 
